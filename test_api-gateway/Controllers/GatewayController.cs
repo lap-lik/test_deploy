837250dd1f922d17d9f0190e85f8b594a4ad3be1
lap-lik <lap-lik@yandex.ru>
2025-09-22 03:09:34 +0300

    first commit

---
+++ test_api-gateway/Controllers/GatewayController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace test_api_gateway.Controllers
+{
+    [ApiController]
+    [Route("api/")]
+    public class GatewayController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetAllPersons()
+        {
+            
+            return Ok("Всё работает!");
+        }
+    }
+}
